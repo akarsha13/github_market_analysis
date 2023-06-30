d991d4d53e4d5a47382de00eadbfa6df7463d775
akarsha13 <105956272+akarsha13@users.noreply.github.com>
2023-07-01 01:04:44 +0530

    Add files via upload

---
+++ github_market_analysis.sql
@@ -0,0 +1,14 @@
+CREATE TABLE GitHubMarketAnalysis (
+  Year INT,
+  Individual_Developers INT,
+  Repositories INT,
+  Active_organizations INT,
+  Number_of_employees INT);
+INSERT INTO GitHubMarketAnalysis (Year, Individual_Developers, Repositories, Active_organizations, Number_of_employees )
+VALUES
+  (2016, 5800000, 19400000, 331000,0),
+  (2017, 24000000, 67000000, 1500000, 700),
+  (2018, 31000000, 96000000, 2100000,0),
+  (2019, 40000000, 4400000, 2900000, 1079),
+  (2020, 56000000, 60000000, 1677,0),
+  (2022, 94000000, 200000000, 5000);
